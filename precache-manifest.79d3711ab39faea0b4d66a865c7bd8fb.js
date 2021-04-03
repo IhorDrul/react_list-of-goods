63142f2a5e43b858ff7a0041cd4429f89f563438
Ihor Drul <druleger@gmail.com>
2021-04-03 16:40:59 +0300

    Updates

---
+++ precache-manifest.79d3711ab39faea0b4d66a865c7bd8fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cfc18fb2ebe68fac46b884a7ea582e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5053deb311cd40865840",
+    "url": "./static/css/main.5a1d1362.chunk.css"
+  },
+  {
+    "revision": "cc9bfed92f9904f96e6d",
+    "url": "./static/js/2.6427c4d6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.6427c4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5053deb311cd40865840",
+    "url": "./static/js/main.75505e0e.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
